2090693a121b04c74ae4a114babe50b9434e8dc1
TuaTuivai <TuaMTuivai@gmail.com>
2023-03-08 11:47:39 -0800

    added id that wasnt passing through my seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE department (
   id INT PRIMARY KEY,
-  name VARCHAR(30),
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
-  id: INT PRIMARY KEY,
+  id INT PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT,
+  department_id INT
 );
 
 CREATE TABLE employee (
@@ -15,6 +15,5 @@ CREATE TABLE employee (
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT,
- 
+  manager_id INT
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,23 @@
 INSERT INTO department
-(name)
+(id, name)
 VALUES
-('Marketing'),
-('Operations'),
-('Human Resources'),
-('Research and Development');
+(1, 'Marketing'),
+(2, 'Operations'),
+(3, 'Human Resources'),
+(4, 'Research and Development');
 
 INSERT INTO role
-(title, salary, department_id)
+(id, title, salary, department_id)
 VALUES
-('Marketing Manager', 105000, 1),
-('Operations Coordinator', 65000, 2),
-('HR Director', 150000, 3),
-('Research Scientist', 85000, 4);
+(1, 'Marketing Manager', 105000, 1),
+(2, 'Operations Coordinator', 65000, 2),
+(3, 'HR Director', 150000, 3),
+(4, 'Research Scientist', 85000, 4);
 
 INSERT INTO employee
-(first_name, last_name, role_id, manager_id)
+(id, first_name, last_name, role_id, manager_id)
 VALUES
-('Tony', 'Gonzalez', 1, 4),
-('Carlos', 'Mencia', 2, 3),
-('Olivia', 'Rodriguez', 3, 1),
-('George', 'Lopez', 4, 5);
+(1, 'Tony', 'Gonzalez', 1, 4),
+(2, 'Carlos', 'Mencia', 2, 3),
+(3, 'Olivia', 'Rodriguez', 3, 1),
+(4, 'George', 'Lopez', 4, 5);
